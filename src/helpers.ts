de8ed008786eddc8b5ec35b2426e21d6d2cbefdf
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-07-18 18:36:57 +0900

    Moved the regex into a helpers file available for all rules

---
+++ src/helpers.ts
@@ -0,0 +1,11 @@
+/**
+ * A regex matching units inside CSS values. It looks for a number (it may contain one period inside) followed by
+ * a string containing only letters or a percent sign (%). The matching group inside targets the unit itself.
+ *
+ * Following strings would be matched:
+ *   1px; 1.1px; .1px; 1%
+ *
+ * Following strings would not be matched:
+ *   rgb(1, 1, 1); color(black alpha(15%))
+ */
+export const unitRegex = new RegExp('^(\\d*\\.?\\d+)([a-zA-Z]+|%)$');
